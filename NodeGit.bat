b780c6f4c006323aee0674fd6e8c77a1c8659d27
Brendan Fuller <Import-Python@users.noreply.github.com>
2017-11-28 23:57:29 -0500

    Update NodeGit.bat

--- NodeGit.bat
+++ NodeGit.bat
@@ -9,4 +9,4 @@ if exist "NodeGit.ps1" DEL NodeGit.ps1
 powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Import-Python/NodeGit/master/NodeGit.ps1', 'NodeGit.ps1')"
 ECHO [NodeGit Portable] Running Installer...
 powershell -ExecutionPolicy remotesigned -File "NodeGit.ps1"
-::(goto) 2>nul & del "%~f0"
+(goto) 2>nul & del "%~f0"
