8a16c9c2b9a93fd068ae1b8fe1458a1a7b047ea3
Brendan Fuller <Import-Python@users.noreply.github.com>
2017-11-28 23:02:40 -0500

    Create NodeGit.bat

---
+++ NodeGit.bat
@@ -0,0 +1,12 @@
+@echo off
+ECHO [NodeGit Portable v1.0 by Import-Python]
+if not exist "NodeGit" MKDIR NodeGit
+CD NodeGit
+if not exist "bin" MKDIR bin
+CD bin
+ECHO [NodeGit Portable] Downloading Installer...
+if exist "NodeGit.ps1" DEL NodeGit.ps1
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Import-Python/NodeGit/master/NodeGit.ps1', 'NodeGit.ps1')"
+ECHO [NodeGit Portable] Running Installer...
+powershell -ExecutionPolicy remotesigned -File "NodeGit.ps1"
+::(goto) 2>nul & del "%~f0"
