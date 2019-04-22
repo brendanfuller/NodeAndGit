1ba39c0d51a76eb40e091b0a3e3e2418f3c5dc0d
Brendan Fuller <13824197+ImportProgram@users.noreply.github.com>
2019-04-22 15:10:10 -0400

    Update version and name

--- NodeGitInterface.bat
+++ NodeGitInterface.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-TITLE NodeGet Interface by Import-Python
-ECHO [NodeGit Interface v1.0 by Import-Python]
+TITLE NodeGet Interface by ImportProgram
+ECHO [NodeGit Interface v1.1 by ImportProgram]
 PATH %PATH%;%cd%\git\cmd;%cd%\node\node
 powershell -ExecutionPolicy ByPass -File "bin/NodeGitInterface.ps1"
